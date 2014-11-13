d75cf270ea778b2cd1e3b83376b9cc56a76c5a39
Peter Samyn <11samype@gmail.com>
2014-11-13 16:27:42 -0500

    done????

--- to_turn_in/example_crate.js
+++ to_turn_in/example_crate.js
@@ -21,6 +21,9 @@ function animate(){
 		
 		camera.position.z -= 2.18;
 		camera.position.y += .02;
+		
+		//camera.applyMatrix( new THREE.Matrix4().makeTranslation( 0, .02, -2.18));
+		
 		cubeAndData.rotation.x -= 0.0008;
 
 	} else if((flapObject1.rotation.y < -Math.PI/4) && !flap1close) {
@@ -54,9 +57,11 @@ function animate(){
 	}
 	
 	if (crateFlyOut) {
-		upsideDownCube.position.y += 5;
+		upsideDownCube.translateY(-5);
 		crashcart.position.y -= 3;
 		crashcart.position.z += 9;
+		
+		//crashcart.applyMatrix( new THREE.Matrix4().makeTranslation( 0, -3, 9));
 		
 		wheel1.rotation.y += 0.02;
 		wheel2.rotation.y += 0.02;
@@ -67,7 +72,7 @@ function animate(){
 			//reset
 			scene.remove(crashcart);
 			scene.remove(upsideDownCube);
-			
+
 			camera.position.x = 0;
 			camera.position.y = 0;
 			camera.position.z = 1100;
@@ -180,6 +185,7 @@ cubeWall1.translateZ(100);
 
 cubeObject.add(cubeWall1);
 
+/*
 var innerWall1 = new THREE.Mesh(new THREE.PlaneBufferGeometry(200, 200), light);
 innerWall1.overdraw = true;
 
@@ -187,6 +193,7 @@ innerWall1.translateY(100);
 innerWall1.rotation.x += 3*Math.PI/2;
 
 cubeObject.add(innerWall1);
+*/
 
 var cubeWall2 = new THREE.Mesh(new THREE.BoxGeometry(200, 200, 1), crate);
 cubeWall2.overdraw = true;
@@ -214,12 +221,11 @@ var flapObject1 = new THREE.Object3D();
 
 var flap1 = new THREE.Mesh(new THREE.BoxGeometry(100, 200, 1), crate_flap);
 flap1.overdraw = true;
-flap1.applyMatrix( new THREE.Matrix4().makeTranslation( 50, 0, 0 ));
+flap1.translateX(50);
 
 flapObject1.add(flap1);
 
-flapObject1.translateX(100);
-flapObject1.translateY(100);
+flapObject1.applyMatrix( new THREE.Matrix4().makeTranslation( 100, 100, 0));
 
 flapObject1.rotation.x = Math.PI/2;
 flapObject1.rotation.y = Math.PI/4;
@@ -230,11 +236,12 @@ var flapObject2 = new THREE.Object3D();
 
 var flap2 = new THREE.Mesh(new THREE.BoxGeometry(100, 200, 1), crate_flap);
 flap2.overdraw = true;
-flap2.applyMatrix( new THREE.Matrix4().makeTranslation( 50, 0, 0 ));
+flap2.translateX(50);
 
 flapObject2.add(flap2);
-flapObject2.translateX(-100);
-flapObject2.translateY(100);
+
+flapObject2.applyMatrix( new THREE.Matrix4().makeTranslation( -100, 100, 0));
+
 flapObject2.rotation.x = Math.PI/2;
 flapObject2.rotation.y = 3*Math.PI/4;
 
@@ -244,11 +251,10 @@ var flapObject3 = new THREE.Object3D();
 
 var flap3 = new THREE.Mesh(new THREE.BoxGeometry(100, 200, 1), crate_flap);
 flap3.overdraw = true;
-flap3.applyMatrix( new THREE.Matrix4().makeTranslation( 50, 0, 0 ));
+flap3.translateX(50);
 
 flapObject3.add(flap3);
-flapObject3.translateY(100);
-flapObject3.translateZ(100);
+flapObject3.applyMatrix( new THREE.Matrix4().makeTranslation( 0, 100, 100));
 flapObject3.rotation.x = Math.PI/4;
 flapObject3.rotation.z = Math.PI/2;
 
@@ -258,11 +264,10 @@ var flapObject4 = new THREE.Object3D();
 
 var flap4 = new THREE.Mesh(new THREE.BoxGeometry(100, 200, 1), crate_flap);
 flap4.overdraw = true;
-flap4.applyMatrix( new THREE.Matrix4().makeTranslation( 50, 0, 0 ));
+flap4.translateX(50);
 
 flapObject4.add(flap4);
-flapObject4.translateY(100);
-flapObject4.translateZ(-100);
+flapObject4.applyMatrix( new THREE.Matrix4().makeTranslation( 0, 100, -100));
 flapObject4.rotation.x = 7*Math.PI/4;
 flapObject4.rotation.z = Math.PI/2;
 
@@ -293,26 +298,23 @@ var crashcart = new THREE.Object3D();
 
 var wheel1 = new THREE.Mesh(new THREE.CylinderGeometry(25, 25, 25, 16, 1, false), wheel);
 wheel1.overdraw = true;
-wheel1.position.y += 35;
-wheel1.position.x += 50;
+wheel1.applyMatrix( new THREE.Matrix4().makeTranslation( 50, 35, 0));
 crashcart.add(wheel1);
 
 var wheel2 = new THREE.Mesh(new THREE.CylinderGeometry(25, 25, 25, 16, 1, false), wheel);
 wheel2.overdraw = true;
-wheel2.position.y += 35;
-wheel2.position.x -= 50;
+
+wheel2.applyMatrix( new THREE.Matrix4().makeTranslation( -50, 35, 0));
 crashcart.add(wheel2);
 
 var wheel3 = new THREE.Mesh(new THREE.CylinderGeometry(25, 25, 25, 16, 1, false), wheel);
 wheel3.overdraw = true;
-wheel3.position.y -= 35;
-wheel3.position.x += 50;
+wheel3.applyMatrix( new THREE.Matrix4().makeTranslation( 50, -35, 0));
 crashcart.add(wheel3);
 
 var wheel4 = new THREE.Mesh(new THREE.CylinderGeometry(25, 25, 25, 16, 1, false), wheel);
 wheel4.overdraw = true;
-wheel4.position.y -= 35;
-wheel4.position.x -= 50;
+wheel4.applyMatrix( new THREE.Matrix4().makeTranslation( -50, -35, 0));
 crashcart.add(wheel4);
 
 var cartbase = new THREE.Mesh(new THREE.BoxGeometry(100, 75, 15), cart);
@@ -324,21 +326,20 @@ var crash = new THREE.Object3D();
 var body = new THREE.Mesh(new THREE.CylinderGeometry(5, 25, 75, 16, 1, false), crashbodycolor);
 body.overdraw = true;
 body.rotation.x += Math.PI/2;
-body.position.z += 75/2;
+body.position.z += 75/2; //???
 
 crash.add(body);
 
 var head = new THREE.Mesh(new THREE.SphereGeometry(20), crashbodycolor);
 head.overdraw = true;
-head.position.z += 75;
+head.translateZ(75);
 
 crash.add(head);
 
 var snout = new THREE.Mesh(new THREE.CylinderGeometry(2, 15, 30, 16, 1, false), crashbodycolor);
 snout.overdraw = true;
 
-snout.position.z += 75;
-snout.position.x += 20;
+snout.applyMatrix( new THREE.Matrix4().makeTranslation( 20, 0, 75));
 snout.rotation.z -= Math.PI/2;
 snout.rotation.x -= Math.PI; // hide seam
 
@@ -347,8 +348,7 @@ crash.add(snout);
 var nose = new THREE.Mesh(new THREE.SphereGeometry(5), crashnosecolor);
 nose.overdraw = true;
 
-nose.position.z += 75;
-nose.position.x += 40;
+nose.applyMatrix( new THREE.Matrix4().makeTranslation( 40, 0, 75));
 
 crash.add(nose);
 
@@ -356,6 +356,56 @@ crashcart.add(crash);
 
 crashcart.rotation.x = -Math.PI/4;
 crashcart.rotation.z = -Math.PI/2;
+crashcart.position.y -= 200;
+
+var mask = new THREE.Mesh(new THREE.BoxGeometry(75, 50, 5), crate_flap);
+mask.overdraw = true;
+
+mask.applyMatrix(new THREE.Matrix4().makeTranslation( -100, 50, 75));
+mask.rotation.y += Math.PI/2;
+mask.rotation.x -= Math.PI/16;
+mask.rotation.z -= Math.PI/16;
+
+crashcart.add(mask);
+
+var leaf1 = new THREE.Mesh(new THREE.BoxGeometry(50, 10, 5), new THREE.MeshBasicMaterial({color:'red'}));
+leaf1.overdraw = true;
+
+leaf1.applyMatrix(new THREE.Matrix4().makeTranslation( -100, 50, 150));
+leaf1.rotation.y += Math.PI/2;
+leaf1.rotation.x -= Math.PI/16;
+
+crashcart.add(leaf1);
+
+var leaf2 = new THREE.Mesh(new THREE.BoxGeometry(50, 10, 5), new THREE.MeshBasicMaterial({color:'blue'}));
+leaf2.overdraw = true;
+
+leaf2.applyMatrix(new THREE.Matrix4().makeTranslation( -100, 65, 145));
+leaf2.rotation.y += Math.PI/2;
+leaf2.rotation.x -= Math.PI/16;
+leaf2.rotation.z -= Math.PI/32;
+
+crashcart.add(leaf2);
+
+var leaf3 = new THREE.Mesh(new THREE.BoxGeometry(50, 10, 5), new THREE.MeshBasicMaterial({color:'orange'}));
+leaf3.overdraw = true;
+
+leaf3.applyMatrix(new THREE.Matrix4().makeTranslation( -100, 80, 140));
+leaf3.rotation.y += Math.PI/2;
+leaf3.rotation.x -= Math.PI/16;
+leaf3.rotation.z -= Math.PI/16;
+
+crashcart.add(leaf3);
+
+var leaf4 = new THREE.Mesh(new THREE.BoxGeometry(50, 10, 5), new THREE.MeshBasicMaterial({color:'green'}));
+leaf4.overdraw = true;
+
+leaf4.applyMatrix(new THREE.Matrix4().makeTranslation( -100, 95, 135));
+leaf4.rotation.y += Math.PI/2;
+leaf4.rotation.x -= Math.PI/16;
+leaf4.rotation.z -= 3*Math.PI/32;
+
+crashcart.add(leaf4);
 
 // add subtle ambient lighting
 var ambientLight = new THREE.AmbientLight(0xbbbbbb);
